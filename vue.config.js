9338e4d40cb07f992c6b9025280034e4fd003c78
liukerui <lkr_lkr@163.com>
2020-03-31 12:54:03 +0800

    新建项目

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+// 解析指令参数
+const argv = JSON.parse(process.env.npm_config_argv);
+
+// console.log("process.env.npm_config_argv值", argv)
+
+const config = {};
+// 获取自定义参数
+let idx = 2;
+const cooked = argv.cooked;
+const length = argv.cooked.length;
+while ((idx += 2) <= length) {
+    config[cooked[idx - 2]] = cooked[idx - 1];
+}
+
+process.env.VUE_APP_PROJECT_ID = config['--projectId']
+console.log("页面ID：" + process.env.VUE_APP_PROJECT_ID)
+
+// VUE 平台配置内容
+module.exports = {
+    productionSourceMap: false
+}
